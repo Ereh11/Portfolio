e22eb18a9523d97a8084c7b69bbc59203eb745ce
‪Hany Abdou Saad‬‏ <Hanysaadstd@gmail.com>
2025-01-17 05:13:34 +0200

    first commit

---
+++ Portfolio Project/js/main.js
@@ -0,0 +1,7 @@
+cvbtn = document.getElementsByClassName("cv-button")[0];
+/**
+ * Select Button and add event listener to open CV
+ */
+cvbtn.addEventListener("click", function() {
+    window.open("https://drive.google.com/file/d/1AG_KezQrcb3xy5ZcHhMpcIUil8SKl23B/view?usp=drive_link");
+});
