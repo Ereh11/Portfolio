fd67e399807e3ada820d0d5319040ff69aa05788
‪Hany Abdou Saad‬‏ <93304653+Ereh11@users.noreply.github.com>
2025-01-19 03:04:18 +0200

    final-done!

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 cvbtn = document.getElementsByClassName("cv-button")[0];
+hireMenBtn = document.getElementsByClassName("btn-design")[0];
+whatsppnBtn = document.getElementsByClassName("btn-design")[1];
 /**
  * Select Button and add event listener to open CV
  */
-cvbtn.addEventListener("click", function() {
-    window.open("https://drive.google.com/file/d/1AG_KezQrcb3xy5ZcHhMpcIUil8SKl23B/view?usp=drive_link");
+console.log(cvbtn);
+cvbtn.addEventListener("click", function () {
+  window.open(
+    "https://drive.google.com/file/d/1AG_KezQrcb3xy5ZcHhMpcIUil8SKl23B/view?usp=drive_link"
+  );
+});
+whatsppnBtn.addEventListener("click", function () {
+  window.open("https://wa.me/01157173830");
+});
+hireMenBtn.addEventListener("click", function () {
+  window.open("https://www.linkedin.com/in/hany-abdou-731b831a1/");
 });
